54d5e3ca5c70bfee2ea78085d697e876c0cd9123
Chanyoung Park <rhdwkrxla68@gmail.com>
2022-06-23 16:15:05 +0900

    feat: 반복 / 조건문 추가

---
+++ loop_if/loop_if.js
@@ -0,0 +1,15 @@
+for (var i = 0; i < 10; i += 1) {
+  console.log(`i: ${i}`);
+}
+
+var num = 0;
+while (num < 10) {
+  console.log(`num: ${num}`);
+  num += 1;
+}
+
+var example = 'test';
+
+if (example == 'test') {
+  console.log('example is test');
+}
